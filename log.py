622a5a03214e4fb4db68fca34ffdaaf88760692a
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2019-03-22 10:03:44 +0530

    Initial working commit

--- log.py
+++ log.py
@@ -2,15 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 BASE_LOGIN_URL = "http://172.16.2.1:1000"
+HEADER_KEY = "41665a725d5a5fb0"
 
-response = requests.get("http://detectportal.firefox.com/success.txt")
+login_url = BASE_LOGIN_URL + "/login?" + HEADER_KEY
+response = requests.get(login_url)
 soup = BeautifulSoup(response.text, "html.parser")
 authenticate = soup.find("input", {"name":"magic"})
-login_url = BASE_LOGIN_URL + "/login?" + authenticate["value"]
-print(login_url)
 
-login_data = {"4Tredir":  "http://172.16.2.1:1000/login?423cd10a8af91a52",
-              "magic":    "453ad60181fe2d2a",
+login_data = {"4Tredir":  login_url,
+              "magic":    authenticate["value"],
               "username": "17bcs1752",
               "password": "geez :)"}
 
